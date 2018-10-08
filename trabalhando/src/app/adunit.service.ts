efa5a8d9ed4eedc63bb0f99d715c8e0dfe39e7be
Israel da Cunha Pereira <pereiraisraelcunha@gmail.com>
2018-10-08 15:27:52 -0300

    commit "trabalhando" 08/10/2018
    parado em passo 13

--- trabalhando/src/app/adunit.service.ts
+++ trabalhando/src/app/adunit.service.ts
@@ -1,9 +1,24 @@
+// adunit.service.ts
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdunitService {
 
-  constructor() { }
+  uri = 'http://localhost:4000/adunits';
+
+  constructor(private http: HttpClient) { }
+
+  addAdUnit(unit_name, unit_price) {
+    const obj = {
+      unit_name: unit_name,
+      unit_price: unit_price
+    };
+    this.http.post(`${this.uri}/add`, obj)
+        .subscribe(res => console.log('Done'));
+  }
+
 }
