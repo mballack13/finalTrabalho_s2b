3c24140a59bff7147bd40f7af792657353dc929d
Matheus Canabarro <matheus.canabarro@hotmail.com>
2018-10-11 16:28:10 -0300

    Hospital Module Created

---
+++ clientside/src/app/hospitals/hospital/hospital.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HospitalComponent } from './hospital.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HospitalComponent]
+})
+export class HospitalModule { }
