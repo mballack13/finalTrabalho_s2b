557c92449ea5eddc9a46db97942a276f15f29084
Matheus Canabarro <matheus.canabarro@hotmail.com>
2018-10-12 20:33:48 -0300

    Commit

--- clientside/src/app/hospitals/hospital/hospital.component.ts
+++ clientside/src/app/hospitals/hospital/hospital.component.ts
@@ -18,6 +18,9 @@ export class HospitalComponent implements OnInit {
   latitude: number = -51;
   longitude: number = -30;
 
+  // latitude: number;
+  // longitude: number;
+
   constructor(private hospitalService: HospitalService) { }
 
   ngOnInit(): void {
@@ -32,6 +35,12 @@ export class HospitalComponent implements OnInit {
     this.latitude = parseFloat(this.latPartes[0]+'.'+this.latPartes[1]);
     this.longPartes = h.longitude.split(',');
     this.longitude = parseFloat(this.longPartes[0]+'.'+this.longPartes[1]);
+
+    // this.latitude = this.parseLatitude(h);
+    // console.log(this.latitude);
+    // this.longitude = this.parseLongitude(h);
+    // console.log(this.longitude);
+
   }
   clearSel(): void {
     this.selectedHospital = null;
@@ -52,4 +61,22 @@ export class HospitalComponent implements OnInit {
     });
   }
 
+  // parseLatitude(h: Hospital): number {
+  //   let latitudex: number;
+  //   let latPartes: string[];
+  //   let latitudeString: string = '-51.';
+  //   latPartes = h.latitude.split(',');
+  //   latitudex = parseFloat(latitudeString+latPartes[1]);
+  //   console.log(latitudex);
+  //   return latitudex;
+  // }
+  // parseLongitude(h: Hospital): number {
+  //   let longitudey: number;
+  //   let longPartes: string[];
+  //   let longitudeString: string = '-30.';
+  //   longPartes = h.longitude.split(',');
+  //   longitudey = parseFloat(longitudeString+longPartes[1]);
+  //   console.log(longitudey);
+  //   return longitudey;
+  // }
 }
